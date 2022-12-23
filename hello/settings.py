90519dca64506b33625c58be782a4901d53364e5
Sushmit <sushmitvats98@gmail.com>
2022-12-23 17:23:20 +0530

    6th

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-57#x30f2)8ay*pog(w^i!#3kg3**u1^v@@-29*qd3k_v5j!5=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
