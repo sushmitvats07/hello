1cfce7231e42f7444e17eb41bfdee91e19fbac5a
Sushmit <sushmitvats98@gmail.com>
2022-12-23 17:37:00 +0530

    7th

--- hello/settings.py
+++ hello/settings.py
@@ -79,13 +79,13 @@ environ.Env.read_env()
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'PASS':env('PASS')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'PASS':env('PASS')
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
