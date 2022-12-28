a9bed7175551773d10c630892f603aab5ac39514
Sushmit <sushmitvats98@gmail.com>
2022-12-28 22:37:15 +0530

    13

--- hello/wsgi.py
+++ hello/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('C:\\Users\\hp\\Desktop\\New folder\\hello')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
 
